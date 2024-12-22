5d806236a35a26fa4b12cd3d858091f4ac9e75ab
Talha Momin <77355191+momintlh@users.noreply.github.com>
2024-12-23 00:33:36 +0500

    feat: cards stuff pretty muc complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import Card from "./components/Card"
 
 function App() {
   return (
-    <div className="flex justify-center items-center border-rose-600 border-2 w-screen h-screen">
-      <div className="flex justify-center items-center border-2 border-white">
-        <Card title="About" description="Who am I?" rotataion="-rotate-[5deg] pos" />
-        <Card title="Work" description="What do I do?" rotataion="" />
-        <Card title="Stuff" description="my yapping" rotataion="rotate-[5deg]" />
+    <div className="flex justify-center items-end border-rose-600 border-2 w-screen h-screen">
+      <div className="flex border-2 border-white translate-y-20">
+        <Card title="About" description="Who am I?" rotation="-rotate-[15deg] translate-x-4" />
+        <Card title="Work" description="What do I do?" rotation="rotate-0 -translate-y-4" />
+        <Card title="Stuff" description="my yapping" rotation="rotate-[15deg] -translate-x-4" />
       </div>
     </div>
   )
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,12 @@
 interface CardProps {
   title: string
   description: string
-  rotataion: string
+  rotation: string
 }
 
-function Card({ title, description, rotataion }: CardProps) {
+function Card({ title, description, rotation }: CardProps) {
   return (
-    <div className={`w-[150px] h-[225px] cursor-pointer select-none bg-gray-100 p-4 border-black border-4 rounded-lg ${rotataion} hover:-translate-y-10 transform transition duration-200 ease-in-out`}>
+    <div className={`w-[150px] h-[225px] cursor-pointer select-none bg-gray-100 p-4 border-black border-4 rounded-lg ${rotation} hover:-translate-y-10 transform transition duration-200 ease-in-out`}>
       <h2 className="text-pink-800 text-center">{title}</h2>
       <p className="text-pink-400 text-sm text-center">{description}</p>
     </div>
