176e470db9f3adbaba73255f7f6db14407534ed7
Talha Momin <77355191+momintlh@users.noreply.github.com>
2025-01-06 23:52:40 +0500

    feat: card dragging

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
+import { DndContext } from "@dnd-kit/core"
 import Card from "./components/Card"
+import DropArea from "./components/DropArea"
 
 function App() {
   return (
     <div className="flex flex-col justify-between items-center  border-rose-600 border-2 overflow-hidden w-screen h-screen">
 
-      <div className="flex justify-center items-center flex-grow">
-        <div className="w-[500px] h-[300px] bg-rose-950 rounded-xl text-center">
-          <h1 className="text-rose-50 text-2xl font-mono">Place you card here</h1>
 
-          <div className="w-[120px] h-[160px] sm:w-[135px] sm:h-[180px] md:w-[150px] md:h-[200px] border-2 border-rose-50 border-dashed rounded-lg mx-auto mt-8 flex items-center justify-center">
-            <p className="text-rose-50">Drop card here</p>
+      <DndContext>
+        <div className="flex justify-center items-center flex-grow">
+          <div className="w-[500px] h-[300px] bg-rose-950 rounded-xl text-center">
+            <h1 className="text-rose-50 text-2xl font-mono">Place you card here</h1>
+
+            <DropArea />
+
           </div>
-
         </div>
-      </div>
 
-      <div className="flex justify-center items-end border-white border-2 translate-y-20">
-        <Card title="About" description="Who am I?" rotation="-rotate-[15deg] translate-x-4" />
-        <Card title="Work" description="What do I do?" rotation="rotate-0 -translate-y-4" />
-        <Card title="Stuff" description="my yapping" rotation="rotate-[15deg] -translate-x-4" />
-      </div>
+        <div className="flex justify-center items-end border-white border-2 translate-y-20">
+          <Card cardId={1} title="About" description="Who am I?" rotation="-rotate-[15deg] translate-x-4" />
+          <Card cardId={2} title="Work" description="What do I do?" rotation="rotate-0 -translate-y-4" />
+          <Card cardId={3} title="Stuff" description="my yapping" rotation="rotate-[15deg] -translate-x-4" />
+        </div>
+      </DndContext>
+
     </div>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,26 @@
+import { useDraggable } from "@dnd-kit/core"
+import { CSS } from '@dnd-kit/utilities'
+
 interface CardProps {
   title: string
   description: string
   rotation: string
+  cardId: number
 }
 
-function Card({ title, description, rotation }: CardProps) {
+function Card({ title, description, rotation, cardId }: CardProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: `card${cardId}`
+  })
+
+  const style = {
+    transform: CSS.Translate.toString(transform)
+  }
+
   return (
-    <div className={`w-[120px] h-[160px] sm:w-[135px] sm:h-[180px] md:w-[150px] md:h-[200px] cursor-pointer select-none bg-gray-100 p-4 border-black border-4 rounded-lg ${rotation} hover:-translate-y-10 transform transition duration-200 ease-in-out`}>
+    <div
+      ref={setNodeRef} style={style} {...listeners} {...attributes}
+      className={`w-[120px] h-[160px] sm:w-[135px] sm:h-[180px] md:w-[150px] md:h-[200px] cursor-pointer select-none bg-gray-100 p-4 border-black border-4 rounded-lg ${rotation} hover:-translate-y-10 transform transition duration-200 ease-in-out`}>
       <h2 className="text-pink-800 text-center">{title}</h2>
       <p className="text-pink-400 text-sm text-center">{description}</p>
     </div>
