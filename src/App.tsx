6f09a01668b723b64e12268a8a125c0c2a6e6a84
Talha Momin <77355191+momintlh@users.noreply.github.com>
2025-08-03 23:59:52 +0500

    feat: simple design!

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,49 @@
-import { DndContext, DragEndEvent } from "@dnd-kit/core";
-import DropArea from "./components/DropArea";
-import GlassCard from "./components/GlassCards";
 import BackgroundCanvas from "./background";
-import { SortableContext } from "@dnd-kit/sortable";
 import { useState } from "react";
 
+
 function App() {
+  const [isMe, setIsMe] = useState(true);
 
-  const [items] = useState(['1', '2', '3']);
-
-  function handleEndDrag(event: DragEndEvent) {
-    const { over } = event;
-    if (!over) return;
-
-    console.log(`width: ${window.innerWidth}`);
-    console.log(`height: ${window.innerHeight}`);
-    
-
-    console.log(over.id);
-  }
 
   return (
     <>
       <BackgroundCanvas />
       <div className="flex flex-col justify-between items-center  border-rose-600 border-2 overflow-hidden w-screen h-screen">
-        <DndContext onDragEnd={handleEndDrag}>
-          <div className="flex justify-center items-center flex-grow">
-            <div
-              className="flex justify-center items-center w-[500px] h-[300px] rounded-xl text-center  bg-blue-500/5 backdrop-blur-[4px] border-blue-200/10 border-[3px] shadow-lg"
-            >
-              <h1 className="text-rose-50 text-2xl font-mono">
+        <div className="flex justify-center items-center flex-grow">
+          <div
+            // className="flex justify-center items-center w-[500px] h-[300px] rounded-xl text-center  bg-blue-500/5 backdrop-blur-[4px] border-blue-200/10 border-[3px] shadow-lg"
+            className="flex flex-col justify-center items-center w-[700px] h-[500px] rounded-xl text-center  bg-white/50 backdrop-blur-[4px] border-white border-[2px] shadow-lg"
+          >
+            <div className="flex flex-row justify-between items-center w-full p-4 border-b-2 border-white rounded-xl">
+              <div>
+                <h1 className="text-black text-[2rem] font-mono font-semibold">
+                  Talha Momin
+                </h1>
+                <h1 className="text-black text-[1.25rem] font-mono  cursor-pointer hover:text-purple-600 hover:underline">
+                  {/* <img src="assets/react.svg" alt="" /> */}
+                </h1>
+                <p className="text-black text-sm italic font-mono font-light">
+                  Cooking Products
+                </p>
+              </div>
+              <p className="text-black text-sm italic font-mono font-light">
+                Dark / Light
+              </p>
+            </div>
+            <div className="flex flex-row justify-evenly w-full p-4 border-b- border-white rounded-xl">
+              <h1 className="text-black text-[1.25rem] font-mono  cursor-pointer hover:text-purple-600 hover:underline">
+                About
               </h1>
-              <DropArea />
+              <h1 className="text-black text-[1.25rem] font-mono  cursor-pointer hover:text-purple-600 hover:underline">
+                Projects
+              </h1>
+            </div>
+            <div className="border-2 border-green-500 w-full h-full" >
             </div>
           </div>
 
-          <div className="flex justify-center items-center -translate-y-12">
-            {/* <SortableContext items={items}> */}
-              <GlassCard
-                cardId={1}
-                title="About"
-                description="Who am I?"
-                rotation="-rotate-[15deg] translate-x-2"
-              />
-              <GlassCard
-                cardId={2}
-                title="Projects"
-                description="What do I do?"
-                rotation="rotate-0 -translate-y-2"
-              />
-              <GlassCard
-                cardId={3}
-                title="Stuff"
-                description="my yapping"
-                rotation="rotate-[15deg] -translate-x-2"
-              />
-            {/* </SortableContext> */}
-          </div>
-        </DndContext>
+        </div>
       </div>
     </>
 
