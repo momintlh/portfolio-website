77af860020e4f5e34e4ac9ea5f64fd222da1f51b
Talha Momin <77355191+momintlh@users.noreply.github.com>
2025-08-04 00:26:59 +0500

    glass is back

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,55 @@
 import BackgroundCanvas from "./background";
 import { useState } from "react";
+import GlassCard from "./components/GlassCards";
+import Card from "./components/Card";
 
 
 function App() {
-  const [isMe, setIsMe] = useState(true);
+  const [isAbout, setIsAbout] = useState(true);
 
 
   return (
     <>
       <BackgroundCanvas />
       <div className="flex flex-col justify-between items-center  border-rose-600 border-2 overflow-hidden w-screen h-screen">
+        <div className="flex flex-col justify-center items-center text-center w-[500px]  py-4 rounded-b-2xl  bg-white/10 backdrop-blur-[4px] border-white/10  border-2">
+          <h1 className="text-white text-[2rem] font-mono font-semibold">
+            Talha Momin
+          </h1>
+          <p className="text-purple-300 text-sm italic font-mono font-light">
+            Cooking Products
+          </p>
+        </div>
+
         <div className="flex justify-center items-center flex-grow">
           <div
-            // className="flex justify-center items-center w-[500px] h-[300px] rounded-xl text-center  bg-blue-500/5 backdrop-blur-[4px] border-blue-200/10 border-[3px] shadow-lg"
-            className="flex flex-col justify-center items-center w-[700px] h-[500px] rounded-xl text-center  bg-white/50 backdrop-blur-[4px] border-white border-[2px] shadow-lg"
+            className="flex flex-col justify-center items-center w-[500px] h-[400px] rounded-xl text-center  border-white/10 border-2 shadow-lg bg-blue-500/5 backdrop-blur-[4px]"
           >
-            <div className="flex flex-row justify-between items-center w-full p-4 border-b-2 border-white rounded-xl">
-              <div>
-                <h1 className="text-black text-[2rem] font-mono font-semibold">
-                  Talha Momin
-                </h1>
-                <h1 className="text-black text-[1.25rem] font-mono  cursor-pointer hover:text-purple-600 hover:underline">
-                  {/* <img src="assets/react.svg" alt="" /> */}
-                </h1>
-                <p className="text-black text-sm italic font-mono font-light">
-                  Cooking Products
-                </p>
-              </div>
-              <p className="text-black text-sm italic font-mono font-light">
-                Dark / Light
-              </p>
-            </div>
-            <div className="flex flex-row justify-evenly w-full p-4 border-b- border-white rounded-xl">
-              <h1 className="text-black text-[1.25rem] font-mono  cursor-pointer hover:text-purple-600 hover:underline">
+
+            <div className="flex flex-row justify-evenly w-full p-4 border-b- border-white/10  border-b-2 rounded-xl">
+              <h1 className="text-white text-[1.25rem] font-mono cursor-pointer hover:text-purple-250 hover:underline" onClick={() => setIsAbout(true)}>
                 About
               </h1>
-              <h1 className="text-black text-[1.25rem] font-mono  cursor-pointer hover:text-purple-600 hover:underline">
+              <h1 className="text-white text-[1.25rem] font-mono cursor-pointer hover:text-purple-250 hover:underline" onClick={() => setIsAbout(false)}>
                 Projects
               </h1>
             </div>
-            <div className="border-2 border-green-500 w-full h-full" >
-            </div>
-          </div>
 
+            {isAbout ? (
+              < div className="w-full h-full flex justify-center items-center" >
+                I am so cool!
+              </div>
+            ) : (
+              <div className="w-full h-full flex justify-center items-center" >
+                <GlassCard cardId={1} description="Top down game made with Unity" rotation="45" title="The Last Tale" />
+                My work is so cool!
+              </div>
+            )
+            }
+          </div>
         </div>
-      </div>
+      </div >
+
     </>
 
   );
