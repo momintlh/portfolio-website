72b1156f90d7ca542bea9087cf560009ef599453
momintlh <77355191+momintlh@users.noreply.github.com>
2025-08-05 16:20:24 +0500

    feat: project part done

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,108 @@ import GlassCard from "./components/GlassCards";
 function App() {
   const [isAbout, setIsAbout] = useState(true);
 
+  function getRandomRotation() {
+    const rotations = [
+      "rotate-0",
+      "rotate-1",
+      "rotate-2",
+      "rotate-3",
+      "rotate-6",
+      "-rotate-1",
+      "-rotate-2",
+      "-rotate-3",
+      "-rotate-6",
+    ];
+    return rotations[Math.floor(Math.random() * rotations.length)];
+  }
+
+  function getRandomBg() {
+    const bgs = [
+      "bg-orange-400/25",
+      "bg-rose-400/15",
+      "bg-green-400/15",
+      "bg-blue-400/20",
+      "bg-yellow-300/20",
+      "bg-purple-400/20",
+      "bg-pink-400/20",
+      "bg-cyan-400/20",
+      "bg-lime-400/20",
+    ];
+    return bgs[Math.floor(Math.random() * bgs.length)];
+  }
+
+  const cardsData = [
+    {
+      cardId: 1,
+      title: "Banana of Doom",
+      description: `Juicy online multiple card combining and collection game
+where AI judges the outcome of your battles!`,
+      bg: "bg-orange-400/25",
+      rotation: "-rotate-6",
+    },
+    {
+      cardId: 2,
+      title: "Autism Launchpad",
+      description:
+        "A proof of concept for detecting Autism using AI. This was my capstone project for my Bachelors",
+      bg: "bg-rose-400/15",
+      rotation: "rotate-0",
+    },
+    {
+      cardId: 3,
+      title: "The Last Tale",
+      description: "Top down wave based game made with Unity",
+      bg: "bg-green-400/15",
+      rotation: "rotate-4",
+    },
+    {
+      cardId: 1,
+      title: "Banana of Doom",
+      description: `Juicy online multiple card combining and collection game
+where AI judges the outcome of your battles!`,
+      bg: "bg-orange-400/25",
+      rotation: "-rotate-2",
+    },
+    {
+      cardId: 2,
+      title: "Autism Launchpad",
+      description:
+        "A proof of concept for detecting Autism using AI. This was my capstone project for my Bachelors",
+      bg: "bg-rose-400/15",
+      rotation: "rotate-4",
+    },
+    {
+      cardId: 3,
+      title: "The Last Tale",
+      description: "Top down wave based game made with Unity",
+      bg: "bg-green-400/15",
+      rotation: "rotate-3",
+    },
+    {
+      cardId: 1,
+      title: "Banana of Doom",
+      description: `Juicy online multiple card combining and collection game
+where AI judges the outcome of your battles!`,
+      bg: "bg-orange-400/25",
+      rotation: "-rotate-1",
+    },
+    {
+      cardId: 2,
+      title: "Autism Launchpad",
+      description:
+        "A proof of concept for detecting Autism using AI. This was my capstone project for my Bachelors",
+      bg: "bg-rose-400/15",
+      rotation: "rotate-1",
+    },
+    {
+      cardId: 3,
+      title: "The Last Tale",
+      description: "Top down wave based game made with Unity",
+      bg: "bg-green-400/15",
+      rotation: "rotate-3",
+    },
+  ];
+
   return (
     <>
       <BackgroundCanvas />
@@ -19,7 +121,11 @@ function App() {
         </div>
 
         <div className="flex justify-center items-center flex-grow">
-          <div className="flex flex-col justify-center items-center w-[500px] h-[400px] rounded-xl text-center  border-white/10 border-2 shadow-lg bg-blue-500/5 backdrop-blur-[4px]">
+          <div
+            className={`flex flex-col justify-center items-center ${
+              isAbout ? "w-[500px] h-[400px] transition-width duration-300" : "w-[600px] h-[400px] transition-width duration-300"
+            } rounded-xl text-center  border-white/10 border-2 shadow-lg bg-blue-500/5 backdrop-blur-[4px]`}
+          >
             <div className="flex flex-row justify-evenly w-full p-4 border-b- border-white/10  border-b-2 rounded-xl">
               <h1
                 className={`text-[1.25rem] font-mono cursor-pointer hover:text-purple-400 hover:underline ${
@@ -48,25 +154,16 @@ function App() {
                 I am so cool!
               </div>
             ) : (
-              <div className="w-full h-full flex justify-center items-center">
-                <GlassCard
-                  className="bg-orange-400/25 -rotate-6"
-                  cardId={1}
-                  description={`Juicy online multiple card combining and collection game\n where AI judges the outcome of your battles!`}
-                  title="Banana of Doom"
-                />
-                <GlassCard
-                  className="bg-rose-400/15 rotate-0"
-                  cardId={2}
-                  description="An proof of concept for detecting Autism using AI.This was my capstone project for my Bachelors"
-                  title="Autism Launchpad"
-                />
-                <GlassCard
-                  className="bg-green-400/15 -rotate-6"
-                  cardId={1}
-                  description="Top down wave based game made with Unity"
-                  title="The Last Tale"
-                />
+              <div className="grid grid-cols-3 gap-4 place-items-center h-full w-full overflow-y-auto p-8">
+                {cardsData.map(({ cardId, title, description }) => (
+                  <GlassCard
+                    key={cardId + title}
+                    cardId={cardId}
+                    title={title}
+                    description={description}
+                    className={`${getRandomBg()} ${getRandomRotation()}`}
+                  />
+                ))}
               </div>
             )}
           </div>
--- src/components/GlassCards.tsx
+++ src/components/GlassCards.tsx
@@ -27,15 +27,13 @@ function GlassCard({ title, description, cardId, className }: CardProps) {
       className={`${className} touch-none rounded-xl border-2 w-28 h-32 sm:w-32 sm:h-44    backdrop-blur-[5px] border-white/15 hover:ring-2
        ${
          !isDragging
-           ? "hover:-translate-y-4 hover:rotate-0 transition-transform duration-200"
+           ? "hover:-translate-y-4 hover:scale-100 hover:rotate-0 transition-transform duration-200"
            : "duration-75"
        }`}
     >
-      <div className="flex flex-col w-full h-full justify-around items-center">
-        <h2 className="text-white text-lg text-center underline">
-          {title}
-        </h2>
-        <p className="text-gray-300 text-sm text-left line-clamp-3 border-2 italic">
+      <div className="flex flex-col justify-center items-center space-y-4">
+        <h2 className="text-white text-lg text-center underline">{title}</h2>
+        <p className="text-gray-300 text-sm text-left line-clamp-3 italic px-2">
           {description}
         </p>
       </div>
